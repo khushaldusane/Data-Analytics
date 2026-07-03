9add728c733d4ec72bfd1525d7b4cda2dc5fb14f
khushaldusane <155304237+khushaldusane@users.noreply.github.com>
2026-07-03 18:08:59 +0530

    Update Python Problems.py

--- Python Problems.py
+++ Python Problems.py
@@ -232,3 +232,13 @@ def average_grade(grades):
  
 grades=[80, 90, 75, 85, 95]
 print(average_grade(grades))
+
+
+#  Write a Python function called find_max that takes a list of numbers as input and returns the maximum number from that list.
+
+def find_max(nums):
+  max_num=max(nums)
+  return max_num
+
+nums=[1,51,44,84,54,62,0]  
+print(find_max(nums))
