79a36b09dee4b041c36d119de5a572994ff567e2
Khushal Dusane <155304237+khushaldusane@users.noreply.github.com>
2026-07-14 22:22:37 +0530

    Update Python Problems.py

--- Python Problems.py
+++ Python Problems.py
@@ -347,3 +347,13 @@ arr = np.array([
 print(arr.sum(axis=0))
 # Sum of columns
 print(arr.sum(axis=1))
+
+#Write a solution to create a DataFrame from a 2D list called student_data. This 2D list contains the IDs and ages of some students.
+#The DataFrame should have two columns, student_id and age, and be in the same order as the original 2D list.
+import pandas as pd
+def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+    return pd.DataFrame(student_data,columns=["student_id","age"])
+
+
+
+  
